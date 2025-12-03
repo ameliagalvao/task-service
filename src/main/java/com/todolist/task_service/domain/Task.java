94ad750a6fe4b7203fc2099b5bfa7af001a1be63
amelia <ameliamgalvao@gmail.com>
2025-12-03 18:26:12 -0300

    refatorando para paradigma reativo.

--- src/main/java/com/todolist/task_service/domain/Task.java
+++ src/main/java/com/todolist/task_service/domain/Task.java
@@ -1,11 +1,13 @@
 package com.todolist.task_service.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @Table("tasks")
+@AllArgsConstructor
 public class Task {
     @Id
     private Long id;
